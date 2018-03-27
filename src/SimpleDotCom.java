f29f1652c0730410c3bf5bdcda6fdfd7f4b0af4a
LiQilong <13673729700@163.com>
2018-03-28 00:00:07 +0800

    Update SimpleDotCom.java

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -5,19 +5,24 @@ public class SimpleDotCom {
         locationCells=locs;
     }
     public String checkYourself(String stringGuess){
+        //把字符串转换成int
         int guess=Integer.parseInt(stringGuess);
+        //创建出保存返回结果的变量。以miss作为默认值
         String result="miss";
+        //以循环对每个格子重复执行
         for (int cell:locationCells){
-            if (guess==cell){
-                result="hit";
-                numOfHits++;
-                break;
-            }
-        }
+            //比较格子与猜测值
+            if (guess==cell){//如果猜中
+                result="hit";//命中
+                numOfHits++;//递增命中数
+                break;//已经离开循环，但需要判断是否击沉
+            }//end if
+        }//end for
+        //如果命中数为3，则返回击沉信息
         if (numOfHits==locationCells.length){
             result="kill";
-        }
-        System.out.println(result);
-        return result;
-    }
+        }//end if 
+        System.out.println(result);//将结果显示出来
+        return result;//将结果返回给调用方
+    }//end method
 }
